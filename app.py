379c458e5c8c974c80f30c147b697e20287cb9b3
amitmactavish <amitmactavish@gmail.com>
2025-05-06 10:09:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,5 +53,9 @@ def download(filetype):
 
     return send_file(file_path, as_attachment=True)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
